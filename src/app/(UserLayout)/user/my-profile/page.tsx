bf9f0205243131b316e387a8af53c62ccfaed848
Abdus Salam Mehedi <salam.mehedi99@gmail.com>
2024-10-10 08:28:38 +0600

    added my-profile page for user dashboard

---
+++ src/app/(UserLayout)/user/my-profile/page.tsx
@@ -0,0 +1,16 @@
+import ProfileDetails from "@/src/app/_components/my-profile/ProfileDetails"
+import { getUserData } from "@/src/services/AuthService"
+import { useToken } from "@/src/utils/useToken"
+
+const MyProfilePage = async () => {
+    const decodedUser = await useToken()
+
+    const { data } = await getUserData(decodedUser?._id!)
+    return (
+        <div className="min-h-screen bg-gradient flex justify-center ">
+            <ProfileDetails userData={data} />
+        </div>
+    )
+}
+
+export default MyProfilePage
